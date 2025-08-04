b905a610874e4aca91004a09ab16b69647d83883
Nathan Boxhall-Burnett <nboxhallburnett@gmail.com>
2025-08-03 18:51:32 -0700

    Add host machine's public IP to the list of 'local' addresses

--- src/lib/admin.js
+++ src/lib/admin.js
@@ -1,4 +1,8 @@
+const dns = require('dns');
 const { BlockList } = require('net');
+
+const config = require('../../conf');
+const log = require('../lib/logger')('admin');
 
 // Yes, we're using "BlockList" as an allow list
 const localAddressList = new BlockList();
@@ -9,6 +13,18 @@ localAddressList.addSubnet('10.0.0.0', 8);
 localAddressList.addSubnet('127.0.0.0', 8);
 localAddressList.addSubnet('172.16.0.0', 12);
 localAddressList.addSubnet('192.168.0.0', 16);
+
+// Attempt to fetch the host machine's IP address from an A record of the host
+dns.resolve4(config.host, (err, addresses) => {
+	if (err) {
+		log('Error resolving IPv4 record: %s', String(err));
+		return;
+	}
+	log('Fetched host machine IPv4 address(es): %o', addresses);
+	for (const address of addresses) {
+		localAddressList.addAddress(address)
+	}
+});
 
 module.exports = {
 	isPrivateIp
